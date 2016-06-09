dfde238fbdddad48dd72b917bbe7751bafa4087e
CallyWally12 <caljam2003@gmail.com>
2016-06-09 20:15:51 +0100

    added cli.js and updated index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+function insect(text) {
+	
+}
+
+function profanity_check(text) {
+  var xhr = new XMLHttpRequest();
+  var uri = parse("http://www.purgomalum.com/service/json?text=%s&add=input&fill_text=****", msg);
+  xhr.open('GET', uri, true);
+  xhr.send()
+  xhr.onreadystatechange = processRequest;
+}
+
+function processRequest(e) {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    var response = JSON.parse(xhr.responseText);
+    msg = response.result;
+
+    insect(msg);
+  }
+}
+
+function parse(str) {
+  var args = [].slice.call(arguments, 1),
+    i = 0;
+
+  return str.replace(/%s/g, function() {
+    return args[i++];
+  });
+}
+
+module.exports = insect;
