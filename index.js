8b3f313259159c3be52132408f3b30d12b87ab9c
Callum Leslie <caljam2003@gmail.com>
2016-06-24 18:08:02 +0100

    started sensetivity checking function and created readfile function

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
+var fs = require('fs');
 
 function insect(flags) {
 	console.log(flags);
 	if(flags.s) {
 		profanity_check(flags.s);
 	}
+	if(flags.f) {
+		readFile(flags.f);
+	}
 }
-	
+
 function profanity_check(msg) {
   var uri = "http://www.purgomalum.com/service/json?text=" + msg + "&add=input&fill_text=****";
   xhr.open('GET', uri, true);
   xhr.send();
+	console.log("check 12")
   xhr.onreadystatechange = processRequest;
 }
 
@@ -20,16 +25,25 @@ function processRequest(e) {
     var response = JSON.parse(xhr.responseText);
     var msg = response.result;
     console.log(msg);
-	something(msg);
+	check_sensetivity(msg);
   }
 }
 
-function check_sensetivity() {
-  
+function check_sensetivity(msg) {
+  var contents = fs.readFileSync("lib/sensetivity.json");
+	var content = JSON.parse(contents);
+
 }
 
 function readFile(filename) {
-	
+	try{
+		var msg = fs.readFileSync(filename);
+		profanity_check(msg);
+	}
+	catch(err) {
+		console.log("Could not find file");
+	}
+
 }
 
 module.exports = insect;
